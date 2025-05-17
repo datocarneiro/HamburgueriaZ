faad1221538e6903405dc2e53df443acc42bc0a7
Dato <datocarneiro1@outlook.com>
2025-05-17 16:28:59 -0300

    update

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.hamburgueriaz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button mButtonSubtrair;
+        Button mButtonAdicionar;
+        EditText mQuantidade;
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mButtonSubtrair = findViewById(R.id.IdButtonSubtrair);
+        mButtonAdicionar = findViewById(R.id.IdButtonAdicionar);
+        mQuantidade = findViewById(R.id.IdQuantidadeNumero);
+
+        mButtonAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String quantidadeTexto = mQuantidade.getText().toString();
+                int quantidade = quantidadeTexto.isEmpty() ? 0 : Integer.parseInt(quantidadeTexto);
+                quantidade++;
+                mQuantidade.setText(String.valueOf(quantidade));
+            }
+        });
+
+        mButtonSubtrair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String quantidadeTexto = mQuantidade.getText().toString();
+                int quantidade = quantidadeTexto.isEmpty() ? 0 : Integer.parseInt(quantidadeTexto);
+                if (quantidade > 0) {
+                    quantidade--;
+                    mQuantidade.setText(String.valueOf(quantidade));
+                }
+            }
+        });
+
+
+
+
+
+
     }
 }
